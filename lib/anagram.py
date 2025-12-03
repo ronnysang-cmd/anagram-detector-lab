faaec33421285900c454eea3d0d37632a563f279
ronnysang <ronny.sang@student.moringaschool.com>
2025-12-03 16:30:12 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, words):
+        return [word for word in words if sorted(word.lower()) == sorted(self.word.lower())]
